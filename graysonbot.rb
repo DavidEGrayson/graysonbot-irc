683c3055a2ce1987527e0d36adbb413df5eb7d8b
David Grayson <davidegrayson@gmail.com>
2016-01-25 22:56:12 -0800

    Removed the root graysonbot.rb.

--- graysonbot.rb
+++
@@ -1,9 +0,0 @@
-require 'openssl'
-require 'socket'
-$LOAD_PATH << 'lib'
-require 'irc_message'
-
-tcp_socket = TCPSocket.new('irc.greynoi.se', 6697)
-socket = OpenSSL::SSL::SSLSocket.new(tcp_socket)
-bot = GraysonBot.new(socket)
-bot.run
